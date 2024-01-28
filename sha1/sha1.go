2aff215af8aff9158755c1bbb273e80bfcdc2fc0
Lukasz Wrzolek <wrzolek.l@pg.com>
2024-01-28 17:27:53 +0100

    File read

---
+++ sha1/sha1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"compress/gzip"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	sig, err := sha1Sum("http.log.gz")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(sig)
+}
+
+// $ cat http.log.gz | gunzip | sha1sum
+func sha1Sum(fileName string) (string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close() // defer is used to execute the function after the surrounding function returns
+	// defer is used to close the file after the function returns, usually used to close the file or file handling
+	r, err := gzip.NewReader(file)
+	if err != nil {
+		return "", err
+	}
+	w := sha1.New()
+
+	if _, err := io.Copy(w, r); err != nil {
+		return "", err
+	}
+
+	sig := w.Sum(nil)
+	return fmt.Sprintf("%x", sig), nil
+}
